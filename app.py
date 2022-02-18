d910037b7ad71b26d49c95556574838177288e95
landonme <landon.me90@gmail.com>
2022-02-17 22:52:22 -0700

    updated app.py text

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from newton_rhapson_weibull_estimation import nwt, get_ci
 
+# to test, go to cmd prompt and run "streamlit run app.py"
+
 
 # Page Layout -- Full Width
 st.set_page_config(page_title='Determining Paramaters of a Weibull distribution using Newton Rhapson estimation of the Maximul Likelihood',
@@ -23,8 +25,15 @@ st.set_page_config(page_title='Determining Paramaters of a Weibull distribution 
 st.title("Determining Paramaters of a Weibull Distribution Using Newton Rhapson")
 st.write("**Created By: [Landon Mecham](https://www.linkedin.com/in/landonme/)**")
 #st.header("Classification Edition")
-st.write("This app uses the Newton Rhapson algorithm built from scratch to find the maximul likelihood estimators of a weibull distribution.")
-st.write("The Newton Raphson algorithm finds the approximate root of a real-valued function using the idea that a continuous differentiable function can be approximated by a straight line tangent to it.")
+st.write("This app uses the Newton Rhapson algorithm built from scratch to find the maximum likelihood estimators of a weibull distribution.")
+st.write("""
+         Maximum Liklihood Estimation is a method for estimating parameters of an assumed probability distribution 
+         given a set of data. It finds the parameter points that maximize the likelihood of randomly observing the data at hand. To do this, it literally finds the parameters
+         that maximize the likelihood function (often converted to the log-likelihood for simplicity).\n
+         
+         In most cases, the first order condition of the likelihood function cannot be solved explicity, and this is where root estimation algorithms like Newton Rhapson come into play.
+         Given a continuous and differentiable function, Newton's method approximates the root by taking a straight line tangent to it, learning from the error, and continuing until convergence.
+         """)
 
 ## Uses Sidebar
 # Input your csv
@@ -40,11 +49,10 @@ st.subheader('Data')
 ## final
 def final_func(data):
     X = data.X     
-    st.markdown('The sample dataset used is the number of years till death or reocurrence after being diagnosed with melanoma')
-    st.write(data.head(5))
+    st.markdown('The sample dataset being used is the number of years till death or reocurrence after being diagnosed with melanoma')
+    st.write(data.X.describe())
     st.write('\n')
     #data['X'].plot(kind="hist", title="Years Till Death or Reoccurence of Melanoma")
-    st.write('\n')
     fw = lambda B: math.fsum(X**B*np.log(X))/math.fsum(X**B) - 1/B - math.fsum(np.log(X))/len(X)
     dfw = lambda B: math.fsum(np.log(X)**2*X**B)/math.fsum(X**B) + 1/B**2 + 1
     B, n = nwt(fw,dfw,2 ,1e-8, 100)
@@ -63,7 +71,7 @@ def final_func(data):
     st.pyplot(fig)
 
 
-# Run Newton Rhapson based off provided dataset
+# final_func
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     final_func(data=data)
@@ -74,4 +82,4 @@ else:
             'https://raw.githubusercontent.com/landonme/Newton-Raphson-and-GLRT/master/melanoma%20years%20till%20death%20or%20relapse.csv')
         final_func(data=data)
         
-        
+     
